5ba326e1a8599d0acb61eccbb23070469878352f
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-01-07 10:55:12 +0100

    created setup
    created setup

---
+++ pm4pygpu/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.0.1'
+__doc__ = "PM4Py GPU"
+__author__ = 'PADS'
+__author_email__ = "alessandro.berti89@gmail.com"
+__maintainer__ = 'PADS'
+__maintainer_email__ = "alessandro.berti89@gmail.com"
---
+++ setup.py
@@ -0,0 +1,27 @@
+from os.path import dirname, join
+
+import pm4pygpu
+from setuptools import setup
+
+
+def read_file(filename):
+    with open(join(dirname(__file__), filename)) as f:
+        return f.read()
+
+
+setup(
+    name="ocel-standard",
+    version=pm4pygpu.__version__,
+    description=pm4pygpu.__doc__.strip(),
+    long_description=read_file('README.md'),
+    author=pm4pygpu.__author__,
+    author_email=pm4pygpu.__author_email__,
+    py_modules=[pm4pygpu.__name__],
+    include_package_data=True,
+    packages=['ocel', 'ocel.exporter', 'ocel.importer', 'ocel.validation'],
+    url='http://www.pm4py.org',
+    license='GPL-3.0',
+    install_requires=[
+        "pm4py"
+    ]
+)
